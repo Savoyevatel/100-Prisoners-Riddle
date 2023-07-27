1509698722ec7c851310ae668f658fe5b2fef664
Savoyevatel <71535298+Savoyevatel@users.noreply.github.com>
2023-07-27 15:49:24 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,57 +1,36 @@
-
 import random
 
-def generate_prisoner(number_prisoners):
-    list = []
+seed = 42
+simulations = 1000
+n_tries = 50
+n_prisoners = 100
+
+outcome = []
+random.seed(seed)
+for x in range(simulations):
+    a = [x for x in range(1,n_prisoners+1)]
+    b = random.sample(a, len(a))
+    def find_number(number, real_number):
+        global count
+        count +=1
+        number = b[number-1]
+        if number == real_number:
+            return number
+        return find_number(number, real_number)
+
+    new = []
     cont = 0
-    while cont < (number_prisoners):
-        cont +=1
-        list.append(cont)
-    return list
+    for i in range(1,n_prisoners+1):
+        count = 0
+        find_number(i, i)
+        if count <= n_tries:
+            new.append(i)
+    if len(new)==n_prisoners:
+        outcome.append(1)
+    else:
+        outcome.append(0)
 
-#Generated prisoners
-a = generate_prisoner(100)
-print(a)
-
-#Hidden prisoners name
-import random
-b = random.sample(a, len(a))
-print(b)
-#count = 0
-
-#real_number = a[prisoner_tag-1]
-def find_number(number, real_number):
-    global count
-    count +=1
-    number = b[number-1]
-    #print(number)
-
-    if number == real_number:
-        return number
-    return find_number(number, real_number)
-
-#Prisoner uses the algorithm and tries to find his box
-#Random prisoner enters the cage
-
-#prisoner_tag = 1
-
-#prisoner_1 = find_number(prisoner_tag, prisoner_tag)
-#print('Prisoner %d found his name after opening %d boxes'%(prisoner_tag,count))
-new = []
-cont = 0
-for i in range(1,101):
-    count = 0
-    find_number(i, i)
-    print('Prisoner %d found his name after opening %d boxes' % (i, count))
-    if count <= 50:
-        new.append(i)
-        #50 % chance to find his name
-        print('Prisoner %d managed to pass'%i)
-
-#print(len(new))
-
-if len(new)==100:
-    print("Prisoners survived!")
-else:
-    print("Prisoners are all executed")
+surviving_prisoners = outcome.count(1)
+print("The number of times prisoners got away is %d in over %s iterations." %(surviving_prisoners, simulations))
+print("The probability of surviving is %s" %(surviving_prisoners/simulations))
 
